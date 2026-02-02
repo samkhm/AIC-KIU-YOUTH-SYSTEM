f5cf6b033c7618acebe0a99b6f2c29237709304a
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2026-02-02 10:16:11 +0300

    status

--- youth-system/src/pages/adminDashboard/components/sections/Gallery.jsx
+++ youth-system/src/pages/adminDashboard/components/sections/Gallery.jsx
@@ -5,7 +5,7 @@ export default function Gallery() {
     <div className="border-l-2 rounded m-5 p-5 h-screen flex flex-col animate__animated animate__zoomIn animate__delay-1s">
     {/* Header */}
       <div className="flex items-center justify-between gap-5 border-b border-gray-200 p-3 sticky top-0 bg-white z-10">
-        <h3 className="text-xl font-semibold">Youth Gallery</h3>
+        <h3 className="text-xl font-semibold">Youth Gallery🎴</h3>
         <button className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded transition">
           Add
         </button>
